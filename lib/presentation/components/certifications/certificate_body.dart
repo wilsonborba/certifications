465316127405a9ac3b41aae29dbdf0df2b62fdc8
Wilson Borba <wilsonmatheuslimaborba@gmail.com>
2026-08-27 03:28:01 +0700

    fix(i18n): import translation extension for certificates

--- lib/presentation/components/certifications/certificate_body.dart
+++ lib/presentation/components/certifications/certificate_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:certifications/core/utils/app_localizations.dart';
 import 'package:flutter/rendering.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
